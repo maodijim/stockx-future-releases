1fdf4c9d06330eb8a53295a955b8a9f91a829e63
Andy <adnymd@gmail.com>
2019-01-21 22:06:29 -0800

    v1.0 Upload scripts

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # stockx-future-releases
+## v1.0
+Prerequisites:
+1. Install Chrome
+2. Download corresponding Chrome webdriver that support your Chrome version from here: http://chromedriver.chromium.org/downloads
+3. Turn on Less secure app access if your are using Gmail account to send email: https://support.google.com/accounts/answer/6010255
+4. Install selenium via pip
+5. Python 3.5 + is required
+
+### Update the following lines in main.py
+```python
+email_server = ""
+email_user = ""
+email_pass = ""
+send_to_email = ""
+```
+
+###Run main.py
+```bash
+python main.py
+```
+
+
+
---
+++ utilities.py
@@ -0,0 +1,78 @@
+import re
+import smtplib
+import argparse
+
+
+def dict_to_string(dict_int):
+    return_str = ""
+    for item in dict_int.items():
+        return_str += """{snk_name}
+    Release Date: {release_date}
+    Release Link: {link}
+    Release Price: {release_price} 
+    Premium: {premium}%
+""".format(snk_name=item[0],
+           release_date=" ".join(item[1]['release_date']),
+           link=item[1]['release_href'],
+           release_price=item[1]['retail_price'],
+           premium=item[1]['price_prem'])
+    return return_str
+
+
+def send_email(smtp_server, email_user, email_password, send_from, send_to, msg,  smtp_port=587):
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.login(email_user, email_password)
+    server.sendmail(send_from, send_to, email_msg_generator(send_from, send_to, msg))
+
+
+def email_msg_generator(send_from, send_to, msg):
+    email_msg = """\
+From:{send_from}
+To:{send_to}
+Subject: Upcoming releases
+
+{msg}
+""".format(send_from=send_from, send_to=send_to, msg=msg)
+    return email_msg
+
+
+def get_args():
+    global args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--email", help="Email that will receive notification")
+    parser.add_argument("--driver-path", help="path to chrome driver path")
+    args = parser.parse_args()
+
+    if not args.driver_path:
+        args.driver_path = input("Please Enter Chrome Webdriver path: ")
+
+    return args
+
+
+def price_extrator(content):
+    """
+    Extrat price from a string
+    :param content: string contains price tag
+    :return: float price
+    """
+    price_regex = re.compile("\$?[0-9.]+")
+    price = 0.0
+    if re.search(price_regex, content):
+        price = float(re.search(price_regex, content).group(0).replace("$", ""))
+    else:
+        print("No price found")
+    return price
+
+
+def percentage_extrator(content_list):
+    """
+    :param content_list: list of gauge-value from stockx
+    :return: float Price Premium percentage
+    """
+    percentage = 0.0
+    for content in content_list:
+        if re.match("[0-9.]+%", content.text):
+            percentage = float(content.text.replace('%', ''))
+    return percentage
